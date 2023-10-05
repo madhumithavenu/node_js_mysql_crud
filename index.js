8710d1550ef23f7277a5f34d298e5e94eb190980
Madhumitha <madhumithavenu03@gmail.com>
2023-10-05 12:34:17 +0530

    Add or edit Employee

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express'),
     app = express();
-    require('express-async-errors');
-    const db = require('./db.js'),
+require('express-async-errors');
+const db = require('./db.js'),
     employeeRoutes = require('./controllers/employee_controller.js');
+app.use(express.json());
+app.use('/api/employees', employeeRoutes);
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.status || 500).send('Something went wrong!')
+})
 
-    app.use('/api/employees', employeeRoutes);
-    app.use((err, req, res, next) => {
-        console.log(err)
-        res.status(err.status || 500).send('Something went wrong!')
-    })
-
-    db.query("SELECT 1")
+db.query("SELECT 1")
     .then(() => {
         console.log('db connection  succeeded.')
         app.listen(3000,
